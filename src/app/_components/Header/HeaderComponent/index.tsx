43dfd7bb4f820e4b6a64a2803df1934a3270b510
vanguyen333 <vanguyen333@yahoo.com>
2023-12-15 13:40:58 -0500

    Created MobileNav

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
+import MobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
-    <nav>
-      <Gutter>
+    <nav className={[classes.header]}>
+      <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo"></img>
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50}></Image>
         </Link>
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
